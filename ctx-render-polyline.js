37e9ea442fd6ae82acfa9d05f330254e06e8c707
Elijah Insua <tmpvar@gmail.com>
2015-10-06 01:38:32 -0400

    add guards around invalid data

--- ctx-render-polyline.js
+++ ctx-render-polyline.js
@@ -1,13 +1,21 @@
 module.exports = renderPolyline;
 
 function renderPolyline(ctx, a) {
+  if (!a) {
+    return;
+  }
+
   var l = a.length;
   var a0 = a[0];
+
+  if (!a0) {
+    return;
+  }
 
   ctx.moveTo(a0[0], a0[1]);
 
   for (var i=1; i<l; i++) {
     var c = a[i]
-    ctx.lineTo(c[0], c[1]);
+    c && ctx.lineTo(c[0], c[1]);
   }
 }
